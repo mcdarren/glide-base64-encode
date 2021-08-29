eecadf70b47236c70c6672afca84116ebac9763b
Darren Murphy <darren@victoriajd.com>
2021-08-29 17:31:20 +0800

    now it should work

--- function.js
+++ function.js
@@ -2,8 +2,7 @@
 window.function = function (url) {
   if (url.value === undefined) return undefined;
   
-  return "fred";
-  return url.value.split('/').slice(-1);
+  return url.value.split('/').slice(-1)[0];
   
 
 }
