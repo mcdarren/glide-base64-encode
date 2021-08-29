5a82e47a624d3f0e529b2c682109134f812d8368
Darren Murphy <darren@victoriajd.com>
2021-08-29 16:44:36 +0800

    Just the filename..

--- function.js
+++ function.js
@@ -1,19 +1,8 @@
 
 window.function = function (url, strip, base64_encode) {
   if (url.value === undefined) return undefined;
-  var strip = strip.value === undefined ? true : strip.value;
-  var encode = base64_encode.value === undefined ? true : base64_encode.value;
   
-  var str = url.value;
-  if (strip === true) {
-    str = url.value.split('/').slice(-1);
-  }
+  return url.value.split('/').slice(-1);
   
-  if (encode === true) {
-    return btoa(str);
-  }
-  else {
-    return str;
-  }
 
 }
