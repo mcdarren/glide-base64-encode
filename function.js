51fa641573ef1816d5ce9da80d0e17a02601d5ab
Darren Murphy <darren@victoriajd.com>
2021-08-29 16:40:19 +0800

    Optional Base64 encode

--- function.js
+++ function.js
@@ -1,20 +1,19 @@
 
-window.function = function (url, strip) {
+window.function = function (url, strip, base64_encode) {
   if (url.value === undefined) return undefined;
-  var strip;
-  if (strip.value === undefined) {
-    strip = false;
-  }
-  else {
-    strip = true;
+  var strip = strip.value === undefined ? true : strip.value;
+  var encode = base64_encode.value === undefined ? true : base64_encode.value;
+  
+  var str = url.value;
+  if (strip === true) {
+    str = url.value.split('/').slice(-1);
   }
   
-  if (strip === true) {
-    var filename = url.value.split('/').slice(-1);
-    return btoa(filename);
+  if (encode === true) {
+    return btoa(str);
   }
   else {
-    return btoa(url.value);
+    return str;
   }
 
 }
